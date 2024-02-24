897697f8c373a7e44930f1830967055388861f46
Gongback <maainnewkin59@gmail.com>
2024-02-24 22:00:25 +0900

    오류 보고서 인식범위 내애만 있어도 피가 닳음 여러 좀비가 겹쳐짐

--- sceen/zombie.gd
+++ sceen/zombie.gd
@@ -5,25 +5,33 @@ var player_chase = false
 var player: CharacterBody2D = null
 var attack_player = null
 var attack_count = 0
+var hp = 100
+@onready var texture_progress_bar = $TextureProgressBar
 
 func _physics_process(delta):
 	print(player_chase)
 	if player_chase:
-		position += (player.position - position)/speed
+		position += ((player.position - position)/speed)
 		print(position)
 	if not attack_player == null and attack_count >= 2:
 		attack_player.hp -= 5
 		attack_count = 0
+		hp -= 10
 	attack_count+=delta
+	if hp == 0:
+		queue_free()
+	texture_progress_bar.value = hp
 
 func _on_area_2d_body_entered(body):
-	player = body
-	player_chase = true
+	if body.name == "Player":
+		player = body
+		player_chase = true
 
 
 func _on_area_2d_body_exited(body):
-	player = null
-	player_chase = false
+	if body.name == "Player":
+		player = null
+		player_chase = false
 
 
 func _on_area_2d_2_body_entered(body):
